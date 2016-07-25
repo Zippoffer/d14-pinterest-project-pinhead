cd342d77cbb7ca8c31e073bdbc273e95205d0683
Matt Latham <latham75@gmail.com>
2016-07-25 14:10:20 -0500

    first controllers, login

---
+++ app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var app = angular.module("PinheadApp", ['ngRoute'])
+	.constant('FirebaseURL', "https://teampinhead-1aff1.firebaseio.com/");
+
+
+
+app.config(function($routeProvider, FireCreds){
+	let authConfig = {
+		apiKey: FireCreds.apiKey,
+		authDomain: FireCreds.authDomain
+	};
+	firebase.initializeApp(authConfig);
+
