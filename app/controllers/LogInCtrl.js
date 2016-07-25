57cf502fef8b6278eacca0ae370187ac1ca92a51
Christopher Hill <thorsdaddy77@gmail.com>
2016-07-25 16:05:56 -0500

    modifications

--- app/controllers/LogInCtrl.js
+++ app/controllers/LogInCtrl.js
@@ -1,23 +1,23 @@
 "use strict";
 
-app.controller("LogInCtrl", function($scope, $location, AuthFactory){
+app.controller("LogInCtrl", function($scope, $location, AuthFactory) {
 
- 
- 
-	$scope.login = function () {
 
-	AuthFactory.authWithProvider()
-    .then(function(result) {
-      var user = result.user.uid;
-      console.log("logged in ", user);
-      // Load to boards?
-      $location.path("/");
-      $scope.$apply();
-    }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-    });
-  };
+
+    $scope.login = function() {
+
+        AuthFactory.authWithProvider()
+            .then(function(result) {
+                var user = result.user.uid;
+                console.log("logged in ", user);
+                // Load to boards?
+                $location.path("/");
+                $scope.$apply();
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+            });
+    };
 });
