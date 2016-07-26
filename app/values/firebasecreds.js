0b4fdfeae57322bde5a8afc718d01763647d3fd0
Christopher Hill <thorsdaddy77@gmail.com>
2016-07-26 10:34:29 -0500

    moves

---
+++ app/values/firebasecreds.js
@@ -0,0 +1,8 @@
+"use strict";
+
+app.constant('FireCreds', {
+    apiKey: "AIzaSyDrwa9aXLnBByG4CzdUz_EgBkfrw-yBzqs",
+    authDomain: "teampinhead-1aff1.firebaseapp.com",
+    databaseURL: "https://teampinhead-1aff1.firebaseio.com",
+    storageBucket: "teampinhead-1aff1.appspot.com",
+});
