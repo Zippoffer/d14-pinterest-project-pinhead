b0b6ff08be48af61f3d531ab7b51e4d5bac21f18
Christopher Hill <thorsdaddy77@gmail.com>
2016-07-26 19:40:40 -0500

    changes that will not take effect

--- app/controllers/NewBoardCtrl.js
+++ app/controllers/NewBoardCtrl.js
@@ -1,17 +1,18 @@
 "use strict";
 
 app.controller("NewBoardCtrl", function($scope, ItemStorage, $location, AuthFactory) {
-  $scope.newBoard = {
-    uid: null,
-    title: "",
-    tags: "",
-  };
+    $scope.newBoard = {
+        uid: null,
+        title: "",
+        tags: "",
+    };
 
-  $scope.addNewBoard = function() {
-    $scope.newBoard.uid = AuthFactory.getUser();
-    ItemStorage.postNewBoard($scope.newBoard)
-      .then(function(response) {
-        $location.url("/pinhead/mainboard");
-      });
-  };
+    $scope.addNewBoard = function() {
+        console.log("you added a new board", $scope.newBoard)
+        $scope.newBoard.uid = AuthFactory.getUser();
+        ItemStorage.postNewBoard($scope.newBoard)
+            .then(function(response) {
+                $location.url("/pinhead/mainboard");
+            });
+    };
 });
--- app/controllers/NewPinCtrl.js
+++ app/controllers/NewPinCtrl.js
@@ -1,20 +1,21 @@
 "use strict";
 
 app.controller("NewPinCtrl", function($scope, ItemStorage, $location, AuthFactory) {
-  $scope.newPin = {
-    uid: null,
-    boardID: "",
-    title: "",
-    url: "",
-    description: "",
-    tags: "",
-  };
+    $scope.newPin = {
+        uid: null,
+        boardID: "",
+        title: "",
+        url: "",
+        description: "",
+        tags: "",
+    };
 
-  $scope.addNewPin = function(boardID) {
-    $scope.newPin.uid = AuthFactory.getUser();
-    ItemStorage.postNewPin($scope.newPin)
-      .then(function(response) {
-        $location.url("/pinhead/board/`${boardID}`");
-      });
-  };
+    $scope.addNewPin = function(boardID) {
+        console.log("you added a new pin", $scope.newPin)
+        $scope.newPin.uid = AuthFactory.getUser();
+        ItemStorage.postNewPin($scope.newPin)
+            .then(function(response) {
+                $location.url("/pinhead/board/`${boardID}`");
+            });
+    };
 });
