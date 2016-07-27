10f190d89e88de650c701c89f6ffc2f426b619db
Matt Latham <latham75@gmail.com>
2016-07-27 12:37:48 -0500

    fixed cancel on pins

--- app/controllers/NewPinCtrl.js
+++ app/controllers/NewPinCtrl.js
@@ -2,25 +2,27 @@
 
 app.controller("NewPinCtrl", function($scope, ItemStorage, $location, $routeParams, AuthFactory) {
 
-    $scope.newPin = {
-        uid: null,
-        boardID: null,
-        date: null,
-        title: "",
-        url: "",
-        description: "",
-        tags: "",
-    };
+  $scope.boardID = $routeParams.boardID
 
-    $scope.addNewPin = function() {
-        $scope.newPin.uid = AuthFactory.getUser();
-        $scope.newPin.boardID = $routeParams.boardID;
-        $scope.newPin.date = Date();
-        ItemStorage.postNewPin($scope.newPin)
-            .then(function(response) {
-                $location.path(`/pinhead/board/${$scope.newPin.boardID}`);
-                ItemStorage.getPins($routeParams.boardID);
-            });
-    };
+  $scope.newPin = {
+    uid: null,
+    boardID: null,
+    date: null,
+    title: "",
+    url: "",
+    description: "",
+    tags: "",
+  };
+
+  $scope.addNewPin = function() {
+    $scope.newPin.uid = AuthFactory.getUser();
+    $scope.newPin.boardID = $routeParams.boardID;
+    $scope.newPin.date = Date();
+    ItemStorage.postNewPin($scope.newPin)
+      .then(function(response) {
+        $location.path(`/pinhead/board/${$scope.newPin.boardID}`);
+        ItemStorage.getPins($routeParams.boardID);
+      });
+  };
 
 });
