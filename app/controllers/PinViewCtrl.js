b349c5c71cf77380e51cee49f9b8d0a070fb7c65
Christopher Hill <thorsdaddy77@gmail.com>
2016-07-27 11:15:25 -0500

    added search to pins, and images link

--- app/controllers/PinViewCtrl.js
+++ app/controllers/PinViewCtrl.js
@@ -1,29 +1,29 @@
 "use strict";
 
-app.controller("PinViewCtrl", function($scope, $routeParams, ItemStorage) {
-  $scope.pins = [];
+app.controller("PinViewCtrl", function($scope, $routeParams, ItemStorage, $location) {
+    $scope.pins = [];
 
-  $scope.boardID = $routeParams.boardID;
+    $scope.boardID = $routeParams.boardID;
 
-  ItemStorage.getPins($routeParams.boardID)
-    .then(function(pinCollection) {
-      $scope.pins = pinCollection;
-
-      $scope.selectedPin = $scope.pins.filter(function(pin) {
-        return pin.id === $routeParams.pinId;
-      })[0];
-    });
-
-  $scope.deletePinCall = function(pin) {
-    ItemStorage.deletePin(pin)
-      .then((pinCollection) => {
-        $scope.pins = pinCollection;
-        $location.path("/partials/board/:boardID");
-        ItemStorage.getPins()
-          .then((pinCollection) => {
+    ItemStorage.getPins($routeParams.boardID)
+        .then(function(pinCollection) {
             $scope.pins = pinCollection;
-          });
-      });
-  };
+
+            $scope.selectedPin = $scope.pins.filter(function(pin) {
+                return pin.id === $routeParams.pinId;
+            })[0];
+        });
+
+    $scope.deletePinCall = function(pin) {
+        ItemStorage.deletePin(pin)
+            .then((pinCollection) => {
+                $scope.pins = pinCollection;
+                $location.path("/partials/board/:boardID");
+                ItemStorage.getPins()
+                    .then((pinCollection) => {
+                        $scope.pins = pinCollection;
+                    });
+            });
+    };
 
 });
